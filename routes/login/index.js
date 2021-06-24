d3e1e122dd8300cc94d687e62b8fb1af32b607c4
zdq <2608075526@qq.comm>
2021-06-25 00:09:35 +0800

    添加权限接口

--- routes/login/index.js
+++ routes/login/index.js
@@ -5,7 +5,7 @@ const settoken = require('../../common/token_vertify');
 const rsaKey = require('../../common/rsaKey');
 
 //登录
-router.post('/', (req, res) => {
+router.post('/', async(req, res) =>{
 	const { account,password } = req.body;
 	if(!account){
 		res.json({code:1,msg:"账号不能为空"});
@@ -16,24 +16,33 @@ router.post('/', (req, res) => {
 	}
 	const pass = rsaKey.decryption(password);
 	const obj = {
-		sql:`select * from account where account= ${ account }`,
+		sql:`SELECT * FROM account WHERE account= '${ account }'`,
 		res
 	}
-	reqSql(obj)
-	.then((result) => {
-		if(result.length == 0 || result[0].password !== pass){
+	try{
+		const result = await reqSql(obj);
+		const item = result[0];
+		if(result.length == 0 || item.password !== pass){
 			res.json({code:1, msg:"账号或密码不正确！"});
 			return false;
 		}else{
-			settoken.setToken(result[0].id,result[0].account).then((data)=>{
-				return res.json({code:0, data:{token:data,id:result[0].id}, msg:"登录成功！"});
-			})
+			const token = await settoken.setToken(item.id,item.account)
+			const obj2 = {
+				sql:`SELECT * FROM user WHERE relId=${item.id}`,
+				res
+			}
+			const result2 = await reqSql(obj2);
+			const item2 = result2[0];
+			return res.json({code:0, data:{token:token,id:item.id,userId:item2.id,name:item2.name}, msg:"登录成功！"});
 		}
-	})
+	}catch{
+		return res.json({code:2, msg:"请求失败！"});
+	}
+		
+	
 })
 
 router.get('/publicKey', (req, res) => {
-	console.log(rsaKey.publicKey)
 	res.json({code:0, data:rsaKey.publicKey});
 })
 
--- routes/user/index.js
+++ routes/user/index.js
@@ -14,12 +14,12 @@ router.post('/list', (req, res) => {
 		res.json({code:1,msg:"页码必须传"});
 		return false;
 	}
-	let sql = `SELECT u.id, u.name, u.phone FROM user as u WHERE 1=1`
+	let sql = `SELECT u.id,a.account,u.name,u.phone,u.roles FROM user u JOIN account a on u.relId=a.id  WHERE 1=1`
 	if(name){
-		sql += ` AND name like '%${name}%'`
+		sql += ` AND u.name like '%${name}%'`
 	}
 	if(phone){
-		sql += ` AND phone like '%${phone}%'`
+		sql += ` AND u.phone like '%${phone}%'`
 	}
 	sql+=` limit ${(pageNum-1)*pageSize} , ${pageSize};SELECT FOUND_ROWS() as total;`
 	const obj = {
@@ -57,7 +57,7 @@ router.put('/updateUser', (req, res) => {
 })
 
 //所有权限
-router.post('/roles', (req, res) => {
+router.get('/roles', (req, res) => {
 	let sql = `SELECT 
 				u1.roleId roleId1, u1.roleName roleName1,u1.parentId parentId1,
 				u2.roleId roleId2, u2.roleName roleName2,u2.parentId parentId2,
@@ -73,16 +73,52 @@ router.post('/roles', (req, res) => {
 		let data = [];
 		for(let i in result){
 			const obj = result[i];
-			data = returnItem([...data],obj,1);
+			data = handleRoles([...data],obj,1);
 		}
-		console.log("data");
-		console.log(JSON.stringify(data));
 		return res.json({code:0, data:data});
 	})
 })
 
+//修改权限
+router.put('/updateRole', (req, res) => {
+	const { userId, roles } = req.body;
+	if(!userId){
+		res.json({code:1,msg:"userId必须传"});
+		return false;
+	}else if(!roles){
+		res.json({code:1,msg:"roles必须传"});
+		return false;
+	}
+	const obj = {
+		sql:`UPDATE user SET roles='${roles}' WHERE id=${userId};`,
+		res
+	}
+	reqSql(obj)
+	.then((result) => {
+		return res.json({code:0, data:{msg:"修改成功！"}});
+	})
+})
 
-function returnItem(arr,obj,index){
+// 根据用户id获取权限
+router.get('/rolesByUserId', (req, res) => {
+	const { userId } = req.query;
+	if(!userId){
+		res.json({code:1,msg:"userId必须传"});
+		return false;
+	}
+	const obj = {
+		sql:`SELECT roles FROM user WHERE id=${userId};`,
+		res
+	}
+	reqSql(obj)
+	.then((result) => {
+		return res.json({code:0, data:result[0].roles});
+	})
+})
+
+
+// 处理权限
+function handleRoles(arr,obj,index){
 	// 先判断是否存在不存在就新增
 	let activeIndex = -1;
 	if(arr.length>0){
@@ -93,7 +129,7 @@ function returnItem(arr,obj,index){
 	// 存在就修改children，不存在就新增
 	if(activeIndex!==-1){
 		if(arr[activeIndex].children){
-			arr[activeIndex].children = returnItem(arr[activeIndex].children,obj,index+1);
+			arr[activeIndex].children = handleRoles(arr[activeIndex].children,obj,index+1);
 		}
 	}else{
 		const child = {
@@ -101,7 +137,7 @@ function returnItem(arr,obj,index){
 			roleName:obj['roleName'+index]
 		}
 		if(index!==3){
-			child.children = returnItem([],obj,index+1);
+			child.children = handleRoles([],obj,index+1);
 		}
 		arr.push(child);
 	}
