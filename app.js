fb1d712d74ef5f0a0fad9873d08b47a2e1c41f54
zdq <2608075526@qq.comm>
2021-08-25 21:13:40 +0800

    优化

--- app.js
+++ app.js
@@ -1,60 +1,83 @@
-var createError = require('http-errors');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var session = require("express-session");
-var logger = require('morgan');
-var vertoken = require('./common/token_vertify');
+const createError = require("http-errors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const logger = require("morgan");
+const vertoken = require("./common/token_vertify");
+const fs = require("fs");
+const accessLog = fs.createWriteStream("./access.log", { flags: "w" });
 
-var loginRouter = require('./routes/login/index');
-var usersRouter = require('./routes/user/index');
+const loginRouter = require("./routes/login/index");
+const usersRouter = require("./routes/user/index");
+const databaseRouter = require("./routes/database/index");
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev")); //打印到控制台
+app.use(logger("combined", { stream: accessLog })); //打印到log日志
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// 配置session
-app.use(session({
-  secret:"sessions", //设置签名秘钥 内容可以任意填写
-  cookie:{ maxAge:60*60*1000 }, //设置cookie的过期时间，例：80s后    session和相应的cookie失效过期
-  resave:true, //强制保存，如果session没有被修改也要重新保存
-  saveUninitialized:false //如果原先没有session那么久设置，否则不设置
-}))
 
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Content-Type,token");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
+  //方便返回json
+  res.header("Content-Type", "application/json;charset=utf-8");
+  if (req.method == "OPTIONS") {
+    //让options请求快速返回
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+// 配置session
+app.use(
+  session({
+    secret: "sessions", //设置签名秘钥 内容可以任意填写
+    cookie: { maxAge: 60 * 60 * 1000 }, //设置cookie的过期时间，例：80s后    session和相应的cookie失效过期
+    resave: true, //强制保存，如果session没有被修改也要重新保存
+    saveUninitialized: false, //如果原先没有session那么久设置，否则不设置
+  })
+);
 
 // 解析token获取用户信息
-app.use(function(req, res, next) {
-  const whiteList = ['/login','/login/publicKey'];
-  if(!whiteList.includes(req.path)){
-    const token = req.headers['token'];
-    console.log(token);
-    if(token == undefined){
-      res.send({ code: -1, msg: 'token不能为空' });
+app.use(function (req, res, next) {
+  const whiteList = ["/login", "/login/publicKey"];
+  if (!whiteList.includes(req.path)) {
+    const token = req.headers["token"];
+    if (token == undefined) {
+      res.send({ code: -1, msg: "token不能为空" });
       return next();
-    }else{
-      vertoken.verToken(token).then((data)=> {
-        req.data = data;
-        return next();
-      }).catch((error)=>{
-        switch (error.name) {
-          case 'JsonWebTokenError':
-            res.send({ code: -1, msg: '无效token' });
-            break;
-          case 'TokenExpiredError':
-            res.send({ code: -1, msg: 'token过期' });
-            break;
-        }
-        return next();
-      })
+    } else {
+      vertoken
+        .verToken(token)
+        .then((data) => {
+          console.log("data");
+          console.log(data);
+          req.data = data;
+          return next();
+        })
+        .catch((error) => {
+          switch (error.name) {
+            case "JsonWebTokenError":
+              res.send({ code: -1, msg: "无效token" });
+              break;
+            case "TokenExpiredError":
+              res.send({ code: -1, msg: "token过期" });
+              break;
+          }
+          return next();
+        });
     }
-  }else{
+  } else {
     return next();
   }
- 
 });
-
-
 
 // //验证token是否过期并规定哪些路由不用验证
 // app.use(expressJwt({
@@ -64,9 +87,8 @@ app.use(function(req, res, next) {
 // 	path: ['/login','/login/publicKey]//除了这个地址，其他的URL都需要验证
 // }));
 
-
-app.use('/login', loginRouter);
-app.use('/user', usersRouter);
-
+app.use("/login", loginRouter);
+app.use("/user", usersRouter);
+app.use("/database", databaseRouter);
 
 module.exports = app;
--- routes/user/index.js
+++ routes/user/index.js
@@ -1,164 +1,164 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const reqSql = require("../../model/index");
 
 //查询列表
-router.post('/list', (req, res) => {
-	const { name = '', phone = '', pageSize, pageNum } = req.body;
-	if(!pageSize){
-		res.json({code:1,msg:"每页条数必须传"});
-		return false;
-	}else if(!pageNum){
-		res.json({code:1,msg:"页码必须传"});
-		return false;
-	}
-	let sql = `SELECT u.id,a.account,a.id accountId,u.name,u.phone,u.roles FROM user u JOIN account a on u.relId=a.id  WHERE 1=1`
-	if(name){
-		sql += ` AND u.name like '%${name}%'`
-	}
-	if(phone){
-		sql += ` AND u.phone like '%${phone}%'`
-	}
-	sql+=` limit ${(pageNum-1)*pageSize} , ${pageSize};SELECT FOUND_ROWS() as total;`
-	const obj = {
-		sql:sql,
-		res
-	}
-	reqSql(obj)
-	.then((result) => {
-		console.log(result)
-		return res.json({code:0, data:{list:result[0],total:result[1][0].total}});
-	})
-})
+router.post("/list", (req, res) => {
+  const { name = "", phone = "", pageSize, pageNum } = req.body;
+  if (!pageSize) {
+    res.json({ code: 1, msg: "每页条数必须传" });
+    return false;
+  } else if (!pageNum) {
+    res.json({ code: 1, msg: "页码必须传" });
+    return false;
+  }
+  let sql = `SELECT SQL_CALC_FOUND_ROWS u.id,a.account,a.id accountId,u.name,u.phone,u.roles FROM user u JOIN account a on u.relId=a.id  WHERE 1=1`;
+  if (name) {
+    sql += ` AND u.name like '%${name}%'`;
+  }
+  if (phone) {
+    sql += ` AND u.phone like '%${phone}%'`;
+  }
+  sql += ` limit ${(pageNum - 1) * pageSize} , ${pageSize};
+			SELECT FOUND_ROWS() as total;`;
+  const obj = {
+    sql: sql,
+    res,
+  };
+  reqSql(obj).then((result) => {
+    return res.json({
+      code: 0,
+      data: { list: result[0], total: result[1][0].total },
+    });
+  });
+});
 
 //修改信息
-router.put('/updateUser', (req, res) => {
-	const { id, name, phone } = req.body;
-	if(!id){
-		res.json({code:1,msg:"id必须传"});
-		return false;
-	}else if(!name){
-		res.json({code:1,msg:"名字必须传"});
-		return false;
-	}else if(!phone){
-		res.json({code:1,msg:"电话必须传"});
-		return false;
-	}
-	const obj = {
-		sql:`UPDATE user SET name='${name}', phone='${phone}' WHERE id=${id};`,
-		res
-	}
-	reqSql(obj)
-	.then((result) => {
-		return res.json({code:0, data:{msg:"修改成功！"}});
-	})
-})
+router.put("/updateUser", (req, res) => {
+  const { id, name, phone } = req.body;
+  if (!id) {
+    res.json({ code: 1, msg: "id必须传" });
+    return false;
+  } else if (!name) {
+    res.json({ code: 1, msg: "名字必须传" });
+    return false;
+  } else if (!phone) {
+    res.json({ code: 1, msg: "电话必须传" });
+    return false;
+  }
+  const obj = {
+    sql: `UPDATE user SET name='${name}', phone='${phone}' WHERE id=${id};`,
+    res,
+  };
+  reqSql(obj).then((result) => {
+    return res.json({ code: 0, data: { msg: "修改成功！" } });
+  });
+});
 
-// 删除用户 
-router.delete('/deleteUser', (req, res) => {
-	const { id, accountId } = req.body;
-	if(!id){
-		res.json({code:1,msg:"用户id必须传"});
-		return false;
-	}else if(!accountId){
-		res.json({code:1,msg:"账户id必须传"});
-		return false;
-	}
-	const obj = {
-		sql:`DELETE FROM user WHERE id=${id};DELETE FROM account WHERE id=${accountId};`,
-		res
-	}
-	reqSql(obj)
-	.then((result) => {
-		return res.json({code:0, data:{msg:"删除成功！"}});
-	})
-})
+// 删除用户
+router.delete("/deleteUser", (req, res) => {
+  const { id, accountId } = req.body;
+  if (!id) {
+    res.json({ code: 1, msg: "用户id必须传" });
+    return false;
+  } else if (!accountId) {
+    res.json({ code: 1, msg: "账户id必须传" });
+    return false;
+  }
+  const obj = {
+    sql: `DELETE FROM user WHERE id=${id};DELETE FROM account WHERE id=${accountId};`,
+    res,
+  };
+  reqSql(obj).then((result) => {
+    return res.json({ code: 0, data: { msg: "删除成功！" } });
+  });
+});
 
 //所有权限
-router.get('/roles', (req, res) => {
-	let sql = `SELECT 
+router.get("/roles", (req, res) => {
+  let sql = `SELECT 
 				u1.roleId roleId1, u1.roleName roleName1,u1.parentId parentId1,
 				u2.roleId roleId2, u2.roleName roleName2,u2.parentId parentId2,
 				u3.roleId roleId3, u3.roleName roleName3,u3.parentId parentId3 
 			 FROM (roles u1 JOIN roles u2 ON u2.parentId=u1.roleId) JOIN roles u3 ON u3.parentId=u2.roleId;`;
-	const obj = {
-		sql:sql,
-		res
-	}
-	reqSql(obj)
-	.then((result) => {
-		// console.log(result)
-		let data = [];
-		for(let i in result){
-			const obj = result[i];
-			data = handleRoles([...data],obj,1);
-		}
-		return res.json({code:0, data:data});
-	})
-})
+  const obj = {
+    sql: sql,
+    res,
+  };
+  reqSql(obj).then((result) => {
+    // console.log(result)
+    let data = [];
+    for (let i in result) {
+      const obj = result[i];
+      data = handleRoles([...data], obj, 1);
+    }
+    return res.json({ code: 0, data: data });
+  });
+});
 
 //修改权限
-router.put('/updateRole', (req, res) => {
-	const { userId, roles } = req.body;
-	if(!userId){
-		res.json({code:1,msg:"userId必须传"});
-		return false;
-	}else if(!roles){
-		res.json({code:1,msg:"roles必须传"});
-		return false;
-	}
-	const obj = {
-		sql:`UPDATE user SET roles='${roles}' WHERE id=${userId};`,
-		res
-	}
-	reqSql(obj)
-	.then((result) => {
-		return res.json({code:0, data:{msg:"修改成功！"}});
-	})
-})
+router.put("/updateRole", (req, res) => {
+  const { userId, roles } = req.body;
+  if (!userId) {
+    res.json({ code: 1, msg: "userId必须传" });
+    return false;
+  } else if (!roles) {
+    res.json({ code: 1, msg: "roles必须传" });
+    return false;
+  }
+  const obj = {
+    sql: `UPDATE user SET roles='${roles}' WHERE id=${userId};`,
+    res,
+  };
+  reqSql(obj).then((result) => {
+    return res.json({ code: 0, data: { msg: "修改成功！" } });
+  });
+});
 
 // 根据用户id获取权限
-router.get('/rolesByUserId', (req, res) => {
-	const { userId } = req.query;
-	if(!userId){
-		res.json({code:1,msg:"userId必须传"});
-		return false;
-	}
-	const obj = {
-		sql:`SELECT roles FROM user WHERE id=${userId};`,
-		res
-	}
-	reqSql(obj)
-	.then((result) => {
-		return res.json({code:0, data:result[0].roles});
-	})
-})
-
+router.get("/rolesByUserId", (req, res) => {
+  const { userId } = req.query;
+  if (!userId) {
+    res.json({ code: 1, msg: "userId必须传" });
+    return false;
+  }
+  const obj = {
+    sql: `SELECT roles FROM user WHERE id=${userId};`,
+    res,
+  };
+  reqSql(obj).then((result) => {
+    return res.json({ code: 0, data: result[0].roles });
+  });
+});
 
 // 处理权限
-function handleRoles(arr,obj,index){
-	// 先判断是否存在不存在就新增
-	let activeIndex = -1;
-	if(arr.length>0){
-		activeIndex = arr.findIndex((item)=>{
-			return obj['roleId'+index] === item.roleId;
-		})
-	}
-	// 存在就修改children，不存在就新增
-	if(activeIndex!==-1){
-		if(arr[activeIndex].children){
-			arr[activeIndex].children = handleRoles(arr[activeIndex].children,obj,index+1);
-		}
-	}else{
-		const child = {
-			roleId:obj['roleId'+index],
-			roleName:obj['roleName'+index]
-		}
-		if(index!==3){
-			child.children = handleRoles([],obj,index+1);
-		}
-		arr.push(child);
-	}
-	return arr;
+function handleRoles(arr, obj, index) {
+  // 先判断是否存在不存在就新增
+  let activeIndex = -1;
+  if (arr.length > 0) {
+    activeIndex = arr.findIndex((item) => {
+      return obj["roleId" + index] === item.roleId;
+    });
+  }
+  // 存在就修改children，不存在就新增
+  if (activeIndex !== -1) {
+    if (arr[activeIndex].children) {
+      arr[activeIndex].children = handleRoles(
+        arr[activeIndex].children,
+        obj,
+        index + 1
+      );
+    }
+  } else {
+    const child = {
+      roleId: obj["roleId" + index],
+      roleName: obj["roleName" + index],
+    };
+    if (index !== 3) {
+      child.children = handleRoles([], obj, index + 1);
+    }
+    arr.push(child);
+  }
+  return arr;
 }
 module.exports = router;
